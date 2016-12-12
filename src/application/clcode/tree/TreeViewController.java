945aad27fdb2e8fd85cf9316e9d62fde84279d01
shinichi666 <shinichi666@x1321551>
2016-12-12 11:19:57 +0900

    CLCodeをTreeViewで表現するクラスのコントローラクラスを作成。

---
+++ src/application/clcode/tree/TreeViewController.java
@@ -0,0 +1,44 @@
+package application.clcode.tree;
+
+import java.io.File;
+import java.util.Optional;
+
+import application.FileChooserManager;
+import application.IOXml;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TreeView;
+
+public class TreeViewController {
+  private FileChooserManager manager = new FileChooserManager("Text Files", "*.xml");
+  private CLTermTree clTermTree;
+
+  // ************************************************************
+  // コンポーネント
+  // ************************************************************
+  @FXML private TreeView<String> treeView;
+  @FXML private Button outputButton;
+  @FXML private Button closeButton;
+
+  @FXML
+  private void initialize() {
+  }
+
+  void setCLCode(String clcode) {
+    clTermTree = new CLTermTree(treeView, clcode);
+  }
+
+  @FXML
+  private void outputButtonOnAction() {
+    Optional<File> fileOpt = manager.saveFile();
+    fileOpt.ifPresent(f -> {
+      IOXml ioXml = new IOXml(clTermTree.getRootValue());
+      ioXml.outputXml(f);
+    });
+  }
+
+  @FXML
+  private void closeButtonOnAction() {
+    closeButton.getScene().getWindow().hide();
+  }
+}
