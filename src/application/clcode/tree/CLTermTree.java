8e7e72c486314bf7718b5603e99ac3813029405b
shinichi666 <shinichi666@x1321551>
2016-12-13 17:20:17 +0900

    最下層を計算し、結果を親ノードに追加して子ノードを削除するメソッドを実装。

--- src/application/clcode/tree/CLTermTree.java
+++ src/application/clcode/tree/CLTermTree.java
@@ -1,5 +1,6 @@
 package application.clcode.tree;
 
+import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import lib.string.combinator.CombinatorLogic;
@@ -8,12 +9,20 @@ import lib.string.combinator.CombinatorLogic;
  * CLTermを管理するTreeViewのマネージャクラス。
  * ファーストクラスコレクションまがい。
  * @author shinichi666
- *
  */
 public class CLTermTree {
   private TreeView<String> treeView;
   private int maxDepth;
   private int depth;
+
+  /**
+   * 木構造クラスとして代用するコンストラクタ。<br>
+   * TreeViewをGUIに配置する予定がないならこのコンストラクタを利用する。
+   * @param clcode CLCode
+   */
+  public CLTermTree(String clcode) {
+    this(new TreeView<>(), clcode);
+  }
 
   /**
    * CLCodeから木構造を生成し、aTreeViewに登録するコンストラクタ。
@@ -53,7 +62,66 @@ public class CLTermTree {
     depth--;
   }
 
-  String getRootValue() {
+  /**
+   * 括弧をたどっていき、一番深い階層から計算を進める。
+   * @param parent
+   */
+  void step(TreeItem<String> parent) {
+    ObservableList<TreeItem<String>> children = parent.getChildren();
+    if (!children.isEmpty()) {
+      if (children.stream().allMatch(c -> c.isLeaf())) {
+        StringBuilder sb = new StringBuilder();
+        children.stream()
+            .map(child -> child.getValue())
+            .forEach(sb::append);
+
+        CombinatorLogic clcode = new CombinatorLogic(sb.toString());
+        while (clcode.canStep()) {
+          clcode.step();
+        }
+        String value = clcode.getValue();
+        if (1 < clcode.getCLTermCount()) {
+          value = "(" + value + ")";
+        }
+        parent.setValue(value);
+        parent.getChildren().removeAll(children);
+        return;
+      }
+
+      children.stream()
+          .filter(c -> !c.isLeaf())
+          .forEach(this::step);
+    }
+  }
+
+  /**
+   * 括弧内のすべての計算を行う。
+   */
+  public void stepAll() {
+    TreeItem<String> root = treeView.getRoot();
+    while (!root.isLeaf()) {
+      step(root);
+    }
+  }
+
+  /**
+   * rootのテキストを返す。
+   * @return rootText
+   */
+  public String getRootValue() {
     return treeView.getRoot().getValue();
+  }
+
+  /**
+   * ルートの一番外の括弧を外す。
+   */
+  public void removeBracket() {
+    String root = getRootValue();
+    if (root.startsWith("(") && root.endsWith(")")) {
+      StringBuilder sb = new StringBuilder(root);
+      sb.deleteCharAt(0);
+      sb.deleteCharAt(sb.length()-1);
+      treeView.getRoot().setValue(sb.toString());
+    }
   }
 }
