d2b4f72d960c2b7d3efd7016d0693a957e679226
shinichi666 <shinichi666@x1321551>
2016-12-13 17:18:48 +0900

    計算が終了した時に括弧内も計算するメソッドを追加
    ダブルクリックでツリーを表示するメソッドをイベントを追加

--- src/application/clcode/tab/CLCodeTableTabController.java
+++ src/application/clcode/tab/CLCodeTableTabController.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 import application.FileChooserManager;
 import application.IOXml;
 import application.MainController;
+import application.clcode.tree.CLTermTree;
 import application.clcode.tree.TreeViewStage;
 import javafx.fxml.FXML;
 import javafx.scene.control.ContextMenu;
@@ -23,6 +24,7 @@ import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.MouseEvent;
 import lib.string.combinator.CombinatorLogic;
+import util.PropertiesKeys;
 
 /**
  * 拡張タブクラスのコントローラクラス。
@@ -86,18 +88,37 @@ public class CLCodeTableTabController {
     tableView.setStyle("-fx-font-size: " + fontSize + "pt;");
   }
 
+  /**
+   * クリックイベント。<br>
+   * <ol>
+   * <li>
+   * <p>
+   * ダブルクリックで選択したCLCodeのツリーを表示
+   * </p>
+   * </li>
+   * </ol>
+   * @param event マウスイベント
+   */
+  @FXML
+  private void tableViewOnMouseClicked(MouseEvent event) {
+    if (event.getClickCount() == 2) {
+      treeMenuItemOnAction();
+    }
+  }
+
   @FXML
   private void contextMenuOnShown() {
     copyMenuItem.setText(MainController.dictionary.getString("tableView-contextMenu-copy"));
     treeMenuItem.setText(MainController.dictionary.getString("tableView-contextMenu-showTreeView"));
-    saveAsXmlMenuItem.setText(MainController.dictionary.getString("tableView-contextMenu-saveAsXml"));
+    saveAsXmlMenuItem
+        .setText(MainController.dictionary.getString("tableView-contextMenu-saveAsXml"));
   }
 
   /**
    * 選択中のレコードのCLCodeをクリップボードにコピー
    */
   @FXML
-  private void copyMenuItemOnClicked() {
+  private void copyMenuItemOnAction() {
     clcodeTable.getSelectedCLCode().ifPresent(clcode -> {
       Clipboard clipboard = Clipboard.getSystemClipboard();
       ClipboardContent content = new ClipboardContent();
@@ -131,16 +152,40 @@ public class CLCodeTableTabController {
   }
 
   /**
-   * クリックイベント。<br>
-   * <ol>
-   *   <li><p>ダブルクリックで選択したCLCodeのツリーを表示</p></li>
-   * </ol>
-   * @param event マウスイベント
+   * 最後のレコードの括弧内を計算する。<br>
+   * コンフィグ設定がオンの時だけ計算を実行する。
    */
-  @FXML
-  private void tableViewOnMouseClicked(MouseEvent event) {
-    if (event.getClickCount() == 2) {
-      treeMenuItemOnAction();
+  private void calculateBracket() {
+    MainController.CONFIG.getProperty(PropertiesKeys.CALCULATION_BRACKET.TEXT)
+        .map(Boolean::valueOf)
+        .filter(b -> b)
+        .ifPresent(calc -> {
+          clcodeTable.getItems()
+              .ifPresent(list -> {
+                int listSize = list.size();
+                Code code = list.get(listSize - 1);
+                String clcode = code.clcodeProperty().get();
+                CLTermTree tree = new CLTermTree(clcode);
+                tree.stepAll();
+                tree.removeBracket();
+
+                // 最後のレコードに新しく追加
+                String resultCLCode = tree.getRootValue();
+                changeColumnWidth(resultCLCode);
+                clcodeTable.addItem(resultCLCode);
+              });
+        });
+  }
+
+  /**
+   * 文字列の長さに合わせてCLCodeカラム幅を変更する。
+   * @param value 文字列
+   */
+  private void changeColumnWidth(String value) {
+    double currentWidth = clcodeColumn.getPrefWidth();
+    double newWidth = value.length() * 10.0;
+    if (currentWidth < newWidth) {
+      clcodeColumn.setPrefWidth(newWidth);
     }
   }
 
@@ -154,21 +199,15 @@ public class CLCodeTableTabController {
   public void setCLCode(String clcode, int max) {
     CombinatorLogic code = new CombinatorLogic(clcode, mainController.getCombinatorsList());
     int step = 0;
-    int width = 0;
     while (code.canStep() && (max <= 0 || step <= max)) {
       step++;
       int cltermCount = code.getCLTermCount();
       String value = code.getValue();
-
-      // 最長文字列の長さに合わせてタブの幅を変更する。
-      int newWidth = value.length() * 10;
-      width = Math.max(width, newWidth);
-      width = Math.max(width, 480);
-      clcodeColumn.setPrefWidth(width);
-
+      changeColumnWidth(value);
       tableView.getItems().add(new Code(step, cltermCount, value));
       code.step();
     }
+    calculateBracket();
   }
 
   void setMainController(MainController aMainController) {
