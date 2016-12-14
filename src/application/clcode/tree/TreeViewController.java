5fc86b0a46836a505666c7e7bd15d20055abe87d
shinichi666 <shinichi666@192.168.11.3>
2016-12-14 21:24:23 +0900

    ボタンのテキストを多言語切り替えに対応

--- src/application/clcode/tree/TreeViewController.java
+++ src/application/clcode/tree/TreeViewController.java
@@ -2,9 +2,12 @@ package application.clcode.tree;
 
 import application.FileChooserManager;
 import application.IOXml;
+import application.MainController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 public class TreeViewController {
   private FileChooserManager manager = new FileChooserManager("Text Files", "*.xml");
@@ -19,6 +22,7 @@ public class TreeViewController {
 
   @FXML
   private void initialize() {
+    setLanguages();
   }
 
   void setCLCode(String clcode) {
@@ -36,5 +40,20 @@ public class TreeViewController {
   @FXML
   private void closeButtonOnAction() {
     closeButton.getScene().getWindow().hide();
+  }
+
+  @FXML
+  private void treeViewOnKeyPressed(KeyEvent event) {
+    if (event.getCode() == KeyCode.ESCAPE) {
+      closeButtonOnAction();
+    }
+  }
+
+  /**
+   * 言語環境に合わせたテキストをセット
+   */
+  private void setLanguages() {
+    outputButton.setText(MainController.dictionary.getString("menu-file-saveXml"));
+    closeButton.setText(MainController.dictionary.getString("common-close"));
   }
 }
